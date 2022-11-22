dc28cc58ad3b39d82433115526e9d029b1f8a9ad
Morgan <Morgan@DESKTOP-48ECI8B>
2022-11-22 13:53:00 +0100

    Initial Commit

---
+++ src/little_prince_2/LittlePrince2.java
@@ -0,0 +1,22 @@
+/** 
+ * LittlePrince2.java						22 Nov 2022 
+ * Morgan Nayet
+ * copyrigths
+ */
+package little_prince_2;
+
+/** TODO commenter ce que fait la classe
+ * @author Morgan Nayet
+ * @version 1.0
+ */
+public class LittlePrince2 {
+
+	/** TODO décrire la méthode
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
